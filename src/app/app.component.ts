c686769df05949bf0b2da4c2bdc5173677477928
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-04-22 20:17:39 -0300

    create big-card style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './nav/nav.component';
 import {TitleComponent} from'./title/title.component';
+import {BigCardComponent} from './big-card/big-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, TitleComponent],
+  imports: [RouterOutlet, NavComponent, TitleComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

426cee935ee69eb068a1a4cc4184358a5ed23aa7
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-04-22 18:13:22 -0300

    add style and creat tittle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './nav/nav.component';
+import {TitleComponent} from'./title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'blog';
 }

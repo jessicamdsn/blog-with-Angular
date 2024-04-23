ab4993c47e04c16c1ccaa4a0e83e4b81ff414ad6
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-04-22 21:39:06 -0300

    create new card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './nav/nav.component';
 import {TitleComponent} from'./title/title.component';
 import {BigCardComponent} from './big-card/big-card.component';
+import {StorieCardsComponent} from './storie-cards/storie-cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, TitleComponent, BigCardComponent],
+  imports: [RouterOutlet, NavComponent, TitleComponent, BigCardComponent, StorieCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/storie-cards/storie-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storie-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './storie-cards.component.html',
+  styleUrl: './storie-cards.component.css'
+})
+export class StorieCardsComponent {
+
+}

675977d1eb5d112b103e9538029dcb22b74705c7
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-04-23 21:25:59 -0300

    add foot component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {TitleComponent} from'./title/title.component';
 import {BigCardComponent} from './big-card/big-card.component';
 import {StorieCardsComponent} from './storie-cards/storie-cards.component';
 import {StorieCardConjuntoComponent} from './storie-card-conjunto/storie-card-conjunto.component';
+import {FootComponent} from './foot/foot.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, TitleComponent, BigCardComponent, StorieCardsComponent, StorieCardConjuntoComponent],
+  imports: [RouterOutlet, NavComponent, TitleComponent, BigCardComponent, StorieCardsComponent, StorieCardConjuntoComponent, FootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
